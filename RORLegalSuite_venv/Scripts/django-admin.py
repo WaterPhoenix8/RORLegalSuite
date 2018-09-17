5d7419e11d734633013118b28a496580f3d72d26
Carlo Durian <techsupport@live.com.ph>
2018-09-17 22:23:55 +0800

    Post-'django-admin startproject'.
    This is the initial commit--no parents. Warning was generated: 'LF will be replaced by CRLF'.

---
+++ RORLegalSuite_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\users\standa~1\docume~1\coding~1\rorleg~1\rorleg~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
