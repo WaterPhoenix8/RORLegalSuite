5e76aa646f9a2e237f01a73f3f402e39c73e82ed
Carlo Durian <techsupport@live.com.ph>
2018-10-02 13:54:13 +0800

    Modified admin.py of the cases app folder: 1. imported all 11 classes/models in the app's models.py 2. then registered all these 11 models in the admin home page

--- RORLegalSuite_root/cases/admin.py
+++ RORLegalSuite_root/cases/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Suffix, Seafarer, LocalAgent, Principal, Correspondent, CaseHandler, Vessel, TypeOfClaim, CounselOfSeafarer, Club, Case
+
+admin.site.register(Suffix)
+admin.site.register(Seafarer)
+admin.site.register(LocalAgent)
+admin.site.register(Principal)
+admin.site.register(Correspondent)
+admin.site.register(CaseHandler)
+admin.site.register(Vessel)
+admin.site.register(TypeOfClaim)
+admin.site.register(CounselOfSeafarer)
+admin.site.register(Club)
+admin.site.register(Case)
+
 # Register your models here.
