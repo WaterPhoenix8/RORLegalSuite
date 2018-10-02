504dd1e9797d07005ebfb257fda4246cb1d8395f
Carlo Durian <techsupport@live.com.ph>
2018-10-02 22:07:41 +0800

    Refined verbose_names(s) in the models.py of class Case.
    This is for cosmetic OR front-end purposes.

--- RORLegalSuite_root/cases/models.py
+++ RORLegalSuite_root/cases/models.py
@@ -14,24 +14,13 @@ class Suffix(models.Model):
 
 class NameField(models.Model):
     """abstract base class for 3 models: Seafarer, Casehandler, and CounselOfSeafarer"""
-    first_name = models.CharField(max_length=50)
-    middle_initial = models.CharField(max_length=4, blank=True)
-    last_name = models.CharField(max_length=35)
+    first_name = models.CharField('First Name', max_length=50)
+    middle_initial = models.CharField('Middle Initial', max_length=4, blank=True)
+    last_name = models.CharField('Last Name', max_length=35)
     suffix = models.ForeignKey(Suffix, on_delete=models.PROTECT, blank=True, null=True)
 
     class Meta:
         abstract = True
-
-
-#class Suffix(models.Model):
-#    """Table 1. Name Suffixes."""
-#    suffix = models.CharField(max_length=5, primary_key=True)
-#
-#    class Meta:
-#        verbose_name_plural = 'suffixes'
-#
-#    def __str__(self):
-#        return self.suffix
 
 
 class Seafarer(NameField):
@@ -53,7 +42,7 @@ class Seafarer(NameField):
 
 class LocalAgent(models.Model):
     """Table 3. Local Agent is the manning agency of the Principal or shipowner."""
-    localagent_name = models.CharField(max_length=100, primary_key=True)
+    localagent_name = models.CharField('Local Agent Name', max_length=100, primary_key=True)
 
     class Meta:
         verbose_name = 'Local Agent'
@@ -65,7 +54,7 @@ class LocalAgent(models.Model):
 
 class Principal(models.Model):
     """Table 4. Principal is also the shipowner."""
-    principal_name = models.CharField(max_length=100, primary_key=True)
+    principal_name = models.CharField("Principal's Name", max_length=100, primary_key=True)
 
     def __str__(self):
         return self.principal_name
@@ -73,7 +62,7 @@ class Principal(models.Model):
 
 class Correspondent(models.Model):
     """Table 5. Correspondents are local port advisers to and assistants of Clubs and Members (or shipowners)."""
-    correspondent_name = models.CharField(max_length=100, primary_key=True)
+    correspondent_name = models.CharField('Correspondent Name', max_length=100, primary_key=True)
 
     def __str__(self):
         return self.correspondent_name
@@ -100,7 +89,7 @@ class CaseHandler(NameField):
 
 class Vessel(models.Model):
     """Table 7. Vessel is the name of the ship."""
-    vessel_name = models.CharField(max_length=75, primary_key=True)
+    vessel_name = models.CharField('Vessel Name', max_length=75, primary_key=True)
 
     def __str__(self):
         return self.vessel_name
@@ -108,7 +97,7 @@ class Vessel(models.Model):
 
 class TypeOfClaim(models.Model):
     """Table 8. Types of Claim."""
-    claim_type = models.CharField('type of claim', max_length=50, primary_key=True)
+    claim_type = models.CharField('Type of Claim', max_length=50, primary_key=True)
 
     class Meta:
         verbose_name = 'Type of Claim'
@@ -137,7 +126,7 @@ class CounselOfSeafarer(NameField):
 class Club(models.Model):
     """Table 10. P&I Club or Protection and Indemnity Insurance Club
      is an association of shipowners for mutual protection."""
-    club_name = models.CharField(max_length=100, primary_key=True)
+    club_name = models.CharField('Club Name', max_length=100, primary_key=True)
 
     def __str__(self):
         return self.club_name
@@ -159,26 +148,32 @@ class Case(models.Model):
 
     seafarer = models.ForeignKey(Seafarer, on_delete=models.PROTECT)
 
-    localagent = models.ForeignKey(LocalAgent, on_delete=models.PROTECT, )
+    localagent = models.ForeignKey(LocalAgent, on_delete=models.PROTECT, verbose_name="Local Agent")
 
     principal = models.ForeignKey(Principal, on_delete=models.PROTECT)
 
-    case_no = models.CharField('case number', max_length=50, blank=True)
+    case_no = models.CharField('Case Number', max_length=50, blank=True)
 
     #max_length of case_title == total max_length of (seafarer's name, len('  vs  '), and local agent)
     #case_title = models.CharField(max_length=200, default='' + '  vs  ' + '' + ', et al.', blank=True)
-    case_title = models.CharField(max_length=255, blank=True,)
+    case_title = models.CharField('Case Title', max_length=255, blank=True,)
     #    default=f"{Seafarer.first_name} {Seafarer.last_name}  vs  {LocalAgent.localagent_name}, et al.",
     #    default=Seafarer.first_name, Seafarer.last_name + '  vs  ' + LocalAgent.localagent_name + ', et al.',
     #    default=f"{seafarer.first_name} {seafarer.last_name}  vs  {localagent.localagent_name}, et al.",
 
     correspondent = models.ForeignKey(Correspondent, on_delete=models.PROTECT, blank=True, null=True)
 
-    casehandler = models.ForeignKey(CaseHandler, on_delete=models.PROTECT, blank=True, null=True)
+    casehandler = models.ForeignKey(
+        CaseHandler,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        verbose_name='Case Handler',
+    )
 
     vessel = models.ForeignKey(Vessel, on_delete=models.PROTECT)
 
-    typeofclaim = models.ForeignKey(TypeOfClaim, on_delete=models.PROTECT)
+    typeofclaim = models.ForeignKey(TypeOfClaim, on_delete=models.PROTECT, verbose_name="Type of Claim")
 
     #contract = models.BooleanField('POEA SEC with CBA?', choices=CBA)
     contract = models.CharField('POEA SEC with CBA?', max_length=20, choices=CBA)
@@ -187,19 +182,19 @@ class Case(models.Model):
 
     #treatment_day1 = models.DateTimeField('1st day of treatment', auto_now_add=True, blank=True, null=True)
     #treatment_day1 = models.DateTimeField('1st day of treatment', blank=True, null=True)
-    treatment_day1 = models.DateField('1st day of treatment', blank=True, null=True)
+    treatment_day1 = models.DateField('1st Day of Treatment', blank=True, null=True)
 
     counselofseafarer = models.ForeignKey(
         CounselOfSeafarer,
         on_delete=models.PROTECT,
-        verbose_name="seafarer's counsel",
+        verbose_name="Seafarer's Counsel",
         blank=True,
         null=True,
     )
 
     club = models.ForeignKey(Club, on_delete=models.PROTECT)
 
-    legal_opinion = models.BooleanField(blank=True, null=True)
+    legal_opinion = models.BooleanField('Legal Opinion Rendered?', blank=True, null=True)
 
     remarks = models.TextField(blank=True)
 
